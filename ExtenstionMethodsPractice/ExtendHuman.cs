1a76c4924b36cc3eae0727542179b93403af5b35
Swdenis <swdenis@mail.ru>
2021-01-12 13:32:29 +0100

    * .gitignore: Initial commit
    * ExtenstionMethodsPractice.sln:
    * Program.cs:
    * ExtendHuman.cs:
    * ExtenstionMethodsPractice.csproj:

---
+++ ExtenstionMethodsPractice/ExtendHuman.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace AmazingExtensions
+{
+    public static class ExtendHuman
+    {
+        public static bool IsDistressCall(this string s) {
+            if (s.Contains("Help!"))
+                return true;
+            else return false;
+
+                }
+    }
+}
---
+++ ExtenstionMethodsPractice/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AmazingExtensions;
+
+namespace ExtenstionMethodsPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Evil clones are wreaking havoc. Help!";
+            Console.Write(message.IsDistressCall());
+
+        }
+    }
+}
